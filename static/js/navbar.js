87ad340a37e0589ab07ee91617b66b0dab4f14fc
Juan D. Pinto <jdpinto1@gmail.com>
2018-12-22 15:03:09 +0200

    Update settings for navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,13 +1,13 @@
 // // When the user scrolls the page, execute myFunction
 // window.onscroll = function() {myFunction()};
-// //
-// // // Get the navbar
+//
+// // Get the navbar
 // var navbar = document.getElementById("navbar");
-// //
-// // // Get the offset position of the navbar
+//
+// // Get the offset position of the navbar
 // var sticky = navbar.offsetTop;
-// //
-// // // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+//
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 // function myFunction() {
 //   if (window.pageYOffset >= sticky) {
 //     navbar.classList.add("sticky")
@@ -15,7 +15,12 @@
 //     navbar.classList.remove("sticky");
 //   }
 // }
+//
+//
+//
 
+
+// Add dropshadow to navbar when scrolling
 
 $(window).scroll(function() {
     if ($(window).scrollTop() > 10) {
