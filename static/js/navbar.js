e3ba1215272e7f83c22dc798035283f34ea091c0
Juan D. Pinto <jdpinto1@gmail.com>
2018-12-23 18:19:36 +0200

    Make navbar more responsive for mobile

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -29,3 +29,11 @@ $(window).scroll(function() {
         $('#navbar').removeClass('floatingnav');
     }
 });
+
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
