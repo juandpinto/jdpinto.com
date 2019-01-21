be1a62a6fd3f69212047a0e29ea719685acad5de
Juan D. Pinto <jdpinto1@gmail.com>
2019-01-21 13:20:57 +0200

    Add paralax image and logo to home page

--- static/js/customjuan.js
+++ static/js/customjuan.js
@@ -84,3 +84,21 @@ window.addEventListener("scroll", function() {
   let offset = window.pageYOffset;
   parallax.style.backgroundPositionY = offset * 0.7 + "px";
 })
+
+
+
+
+$(function () {
+  oldWord = 0;
+  newWord = 0;
+  wordsArray = ["husband", "father", "believer", "educator", "teacher", "nerd", "geek", "scholar", "learner", "polyglot", "techie", "reader", "writer", "adventurer", "traveler", "friend", "brother", "creator", "coder"];
+  setInterval(function () {
+    while (newWord == oldWord){
+      newWord = Math.floor(Math.random() * 19);
+    }
+    oldWord = newWord;
+    $("#changingWord").fadeOut(400, function () {
+      $(this).text(wordsArray[newWord % wordsArray.length]).fadeIn(400);
+    });
+  }, 2000);
+});
