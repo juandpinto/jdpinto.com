9c5225e30b8b0569d25d8e93e2017c41e993afe1
Juan D. Pinto <jdpinto1@gmail.com>
2019-01-21 11:36:50 +0200

    Add parallax intro to index

--- static/js/customjuan.js
+++ static/js/customjuan.js
@@ -74,3 +74,13 @@ $(document).ready(function() {
     }
   });
 });
+
+
+
+
+const parallax = document.getElementById("parallax-div");
+
+window.addEventListener("scroll", function() {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+})
