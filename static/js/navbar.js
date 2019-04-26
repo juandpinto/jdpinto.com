54bc7571fa167f09c3a558e0c6fcd2051ae67f1f
Juan D. Pinto <jdpinto1@gmail.com>
2019-04-26 22:18:19 +0300

    Use CSS for navbar logo hover

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -37,11 +37,3 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
-
-
-$(document).ready(function(){
-    $("#navImage").hover(
-        function() {$(this).attr("src","/images/site/logo_small.png");},
-        function() {$(this).attr("src","/images/site/logo_small_light.png");
-    });
-});
